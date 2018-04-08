287cee350668a3a99d3ed5d8985d6c15dd440b0d
guirn222 <guirnogueira222@gmail.com>
2018-04-08 17:32:36 -0300

    ex 6 finalizado

--- lista 3/L3-E6.py
+++ lista 3/L3-E6.py
@@ -1,2 +1,14 @@
-for x in range(5.00,1.00,-0.5):
-    print (x)
+c=0
+alto = 0
+valor = 0
+for x in range(50,5,-5):
+    x=x/10
+    vl=(120+(c*26))*x-200
+    print ("pelo valor de R$", x , "o locro estimado e de R$", vl)
+    c=c+1
+    if vl > alto :
+        alto = vl
+        valor = x
+        
+
+print ("\no valor mais alto conseguido com o espetaculo e: R$", alto , "\ncom o valor de : R$", valor, "por ingresso")
